c431f3ae78d2499fc9706eb6390621f43e184c57
raymondproguy <raymondnicholas833@gmail.com>
2026-02-16 02:02:52 +0100

    feat: user types defination

---
+++ core/models.go
@@ -0,0 +1,27 @@
+package core
+
+import "time"
+
+//User represents a user in the system
+type User struct {
+	ID            string
+	Email         string
+	PasswordHash  string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+//Session represents a user Session
+type Session struct {
+	ID            string
+	UserID        string
+	RefreshToken  string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+//contains access and refresh tokens for a user
+type TokenPair struct {
+	AccessToken   string
+	RefreshToken  string
+}
