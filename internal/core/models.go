7a3a0e8f2e387d77259c148703c86d9ddf0ca9bc
raymondproguy <raymondnicholas833@gmail.com>
2026-03-02 18:20:50 +0100

    feat(auth): JWT  claims types

--- internal/core/models.go
+++ internal/core/models.go
@@ -1,6 +1,9 @@
 package core
 
-import "time"
+import (
+	"time"
+	"github.com/golang-jwt/jwt/v5"
+)
 
 // User represents a user in the system
 type User struct {
@@ -23,6 +26,16 @@ type Session struct {
 
 // contains access and refresh tokens for a user
 type TokenPair struct {
-	AccessToken  string
-	RefreshToken string
+	AccessToken  string `json: "access_token"`
+	RefreshToken string `json: "refresh_token"`
+	TokenType    string `json: "token_type"`
+	ExpiresIn    int64  `json: "expires_in"`
 }
+
+// Claims represents JWT claims
+type Claims struct {
+	UserID string `json: "user_id"`
+	jwt.RegisteredClaims
+}
+
+
